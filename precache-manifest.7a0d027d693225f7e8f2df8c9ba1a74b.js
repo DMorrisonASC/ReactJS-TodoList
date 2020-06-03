cc222e9590f3a9e234f743d3ba8ae22408be1fd1
Daeshaun <daeshauncodes@gmail.com>
2020-06-03 11:10:53 -0400

    Updates

---
+++ precache-manifest.7a0d027d693225f7e8f2df8c9ba1a74b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c66e059e0f6064e919b74a43adcf199c",
+    "url": "/ReactJS-TodoList/index.html"
+  },
+  {
+    "revision": "4384dec6ee1273e5bbb5",
+    "url": "/ReactJS-TodoList/static/css/main.c0d640a3.chunk.css"
+  },
+  {
+    "revision": "e97e5edef2002bb80d5a",
+    "url": "/ReactJS-TodoList/static/js/2.5d26ceff.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/ReactJS-TodoList/static/js/2.5d26ceff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4384dec6ee1273e5bbb5",
+    "url": "/ReactJS-TodoList/static/js/main.df010e75.chunk.js"
+  },
+  {
+    "revision": "94ae0bc70602659f6b7b",
+    "url": "/ReactJS-TodoList/static/js/runtime-main.8841cfe1.js"
+  }
+]);
