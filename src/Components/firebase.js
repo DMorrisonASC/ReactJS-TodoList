3daf9d79bc6d8502b8d718f7afd6ca9af7d3e09f
DMorrisonASC <52674435+DMorrisonASC@users.noreply.github.com>
2024-03-01 12:15:01 -0500

    Update firebase.js

--- src/Components/firebase.js
+++ src/Components/firebase.js
@@ -1,13 +1,6 @@
 import firebase from "firebase"
 const config = {
-    apiKey: "AIzaSyBfKIgcdralsTcFirx40RrfbgL_APKHp3Y",
-    authDomain: "react-todo-list-1d0f8.firebaseapp.com",
-    databaseURL: "https://react-todo-list-1d0f8.firebaseio.com",
-    projectId: "react-todo-list-1d0f8",
-    storageBucket: "react-todo-list-1d0f8.appspot.com",
-    messagingSenderId: "81120153975",
-    appId: "1:81120153975:web:7eadb86517a58870459172",
-    measurementId: "G-P1WSJDDNNW"
+
 };
 // Initialize Firebase
 firebase.initializeApp(config);
